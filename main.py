09a09fc22c70b2e0b0558b25af2393c74101b29b
Vitomed <viktor.medyankin@mail.ru>
2020-02-02 14:40:04 +0300

    done

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from xmlschema.exceptions import XMLSchemaKeyError
 from xml.etree.ElementTree import ParseError
 
 LOG = "log.txt"
-PATH_XML = "file.xml"
-PATH_XSD = "schema.xsd"
-PATH_XSLT = "style.xsl"
-RESULT_FILE = "resulting.xml"
+XML = "file.xml"
+XSD = "schema.xsd"
+XSL = "style.xsl"
+FILE = "resulting.xml"
 
 
 def logger(func):
@@ -17,12 +17,13 @@ def logger(func):
     def wrapper(*args, **kwargs):
         global log
         try:
-            log.write(f"Call method {func.__name__}, {args}, {kwargs}\n\n")
+            log.write(f"Call method {func.__name__}:  {args}, {kwargs}\n\n")
         except NameError:
             raise NameError(f"You must open a log file for recording")
         r = func(*args, **kwargs)
-        log.write(f"{func.__name__} method is return, {r}\n\n")
+        log.write(f"{func.__name__} method is return: {r}\n\n")
         return r
+
     return wrapper
 
 
@@ -38,28 +39,31 @@ class Validator:
             schema = xmlschema.XMLSchema(self.xsd_path)
             resp = schema.is_valid(xml_path)
         except ParseError as parser_exc:
-            return f"File {os.path.basename(self.xsd_path)} schema is incorrectly composed: {parser_exc}"
+            return f"File {os.path.basename(self.xsd_path)} " \
+                   f"schema is incorrectly composed: {parser_exc}"
         except XMLSchemaKeyError as key_exc:
-            return f"{os.path.basename(xml_path)} does not match file schema: {key_exc}"
+            return f"{os.path.basename(xml_path)}" \
+                   f" does not match file schema: {key_exc}"
         return resp
 
 
 class Transformer:
 
-    def __init__(self, xslt_path: str, **kwargs):
-        self.xslt_path = xslt_path
+    def __init__(self, xsl_path: str, **kwargs):
+        self.xsl_path = xsl_path
         super().__init__(**kwargs)
 
     @logger
-    def transform(self, xlm_path: str, result_file: str) -> [None, str]:
+    def transform(self, xlm_path: str, file: str) -> str:
         dom = parse(xlm_path)
         try:
-            xslt = parse(self.xslt_path)
+            xsl_parse = parse(self.xsl_path)
         except XMLSyntaxError as exc:
-            return f"Error in file syntax {os.path.basename(result_file)}: {exc}"
-        transform = XSLT(xslt)
+            return f"Error in file syntax {os.path.basename(file)}: {exc}"
+        transform = XSLT(xsl_parse)
         result = transform(dom)
-        xsl_file = open(result_file, "wb")
+
+        xsl_file = open(file, "wb")
         xsl_file.write(result)
         xsl_file.close()
         return "Success"
@@ -72,8 +76,8 @@ class Handler(Validator, Transformer):
 
 if __name__ == "__main__":
     log = open(f"{LOG}", "w")
-    h = Handler(xsd_path=PATH_XSD, xslt_path=PATH_XSLT)
-    h.validation(xml_path=PATH_XML)
-    h.transform(xlm_path=PATH_XML, result_file=RESULT_FILE)
-    h.validation(xml_path=RESULT_FILE)
+    h = Handler(xsd_path=XSD, xsl_path=XSL)
+    h.validation(xml_path=XML)
+    h.transform(xlm_path=XML, file=FILE)
+    h.validation(xml_path=FILE)
     log.close()
